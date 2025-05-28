f6bf9d37d60ca9c3acc35fabf2c89ba907e33425
Dorsa Moradi <dorsamoradi2016@gmail.com>
2025-05-28 23:34:57 +0200

    lade till react integration med backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [books, setBooks] = useState([]);
+  const [movies, setMovies] = useState([]);
 
+  useEffect (()=>{
+    //Hämta böcker
+    fetch('http://localhost:7026/api/books')
+    .then(Response => response.json())
+    .then(data => setBooks(data))
+      .catch(error => console.error('Fel vid hämtning av böcker:', error));
+
+      //Hämta filmer 
+      fetch('http://localhost:7026/api/movies')
+      .then(response => response.json())
+      .then(data => setMovies(data))
+      .catch(error => console.error('Fel vid hämtning av filmer:', error));
+  }, []);
+ 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Books</h1>
+      <ul>
+        {books.map(book => (
+          <li key={book.id}>{book.title}</li>
+        ))}
+      </ul>
+
+      <h1>Movies</h1>
+      <ul>
+        {movies.map(movie => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
